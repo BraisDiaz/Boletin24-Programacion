c2c6e2a128a54647a1073289daddea0ae958a6cd
BraisDiaz <braisdiaz@hotmail.com>
2017-04-20 09:24:05 +0200

    Boletin 24 terminado

---
+++ src/boletin24programacion/Boletin24programacion.java
@@ -0,0 +1,22 @@
+
+package boletin24programacion;
+
+/**
+ *
+ * @author Brais Núñez
+ */
+public class Boletin24programacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Ventana1 ven1 = new Ventana1();
+        ven1.setLocationRelativeTo(null);
+        ven1.setVisible(true);
+        
+        
+    }
+    
+}
